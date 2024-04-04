189d3144cf0becb2efc6d79110c51dd2f68b25f0
Leonardo Guerrero <leo.19-20@hotmail.com>
2024-04-04 14:53:29 -0500

    app

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import cors from "cors";
-// import { mainRouter } from "./routes";
+import { mainRouter } from "../routes";
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// app.use( mainRouter );
+app.use( mainRouter );
 
 export { app }
