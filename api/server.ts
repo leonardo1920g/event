b96a09f8a7b0aaa7da633d907a246821402a7b06
Leonardo Guerrero <leo.19-20@hotmail.com>
2024-04-04 14:24:29 -0500

    first commit

---
+++ api/server.ts
@@ -0,0 +1,3 @@
+import { app } from "./src/app"
+
+app.listen(3003, () => console.log("Levantado en el puerto 3003"));
---
+++ api/src/app.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import cors from "cors";
+// import { mainRouter } from "./routes";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// app.use( mainRouter );
+
+export { app }
